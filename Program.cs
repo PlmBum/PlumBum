c6458c85e7e4789cf4f7d4067b5c9c8ab984fab9
PavelYudin303 <paveludin321@gmail.com>
2024-01-23 23:23:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlumBum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 13;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
